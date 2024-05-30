57ec578bf1ee2ecc9de8b0cca623923c212dc3ad
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-05-30 15:25:55 +0200

    Create ScannerFactory.php

---
+++ src/ScannerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bermuda\ClassScanner;
+
+use Psr\Container\ContainerInterface;
+use function Bermuda\Config\conf;
+
+final class ScannerFactory
+{
+    public static function fromContainer(ContainerInterface $container): Scanner
+    {
+        $config = conf($container);
+        if ($config->offsetGet('app')->offsetExists(self::listeneresConfigKey)) {
+            $config = $config->offsetGet('app');
+            return new Scanner(
+                $config->get(static::listeneresConfigKey),
+                $container->get(ClassFinderInterface::class)
+            );
+        }
+
+        return new Scanner(finder: $container->get(ClassFinderInterface::class));
+    }
+}
