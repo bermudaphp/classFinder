8ac2e3f87bc51224ca5fc50dfb3fa09fcca6102d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-31 00:56:31 +0200

    Update ConcreteClassFilter.php

--- src/filter/ConcreteClassFilter.php
+++ src/filter/ConcreteClassFilter.php
@@ -2,8 +2,15 @@
 
 namespace Bermuda\ClassScanner\Filter;
 
+use Bermuda\Reflection\ReflectionClass;
+
 class ConcreteClassFilter extends AbstractFilter
 {
+      /**
+     * @template T
+     * @param class-string<T>
+     * @param iterable<ReflectionClass> $classes
+     */
     public function __construct(
         public readonly string $class,
         iterable $classes = []
@@ -11,7 +18,7 @@ class ConcreteClassFilter extends AbstractFilter
         parent::__construct($classes);
     }
 
-    protected function filter(\ReflectionClass $class): bool
+    protected function filter(ReflectionClass $class): bool
     {
         return $class->name === $this->class;
     }
