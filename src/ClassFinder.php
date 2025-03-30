8a9bfde5b51aa058a7dcd3ddcc564032a2201e18
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-30 23:28:58 +0200

    Update ClassFinder.php

--- src/ClassFinder.php
+++ src/ClassFinder.php
@@ -5,14 +5,70 @@ namespace Bermuda\ClassScanner;
 use PhpParser\Node;
 use PhpParser\NodeFinder;
 use PhpParser\ParserFactory;
+use Bermuda\ClassScanner\Filter\FilterInterface;
 
 final class ClassFinder implements ClassFinderInterface
 {
+    const int MODE_FIND_INTERFACES = 1;
+    const int MODE_FIND_CLASSES = 2;
+    const int MODE_FIND_ENUMS = 3;
+    const int MODE_FIND_TRAITS = 4;
+    const int MODE_FIND_ALL = self::MODE_FIND_CLASSES | self::MODE_FIND_ENUMS | self::MODE_FIND_TRAITS | self::MODE_FIND_INTERFACES;
+
+    /**
+     * @var FilterInterface[]
+     */
+    private array $filters = [];
+
+    public function __construct(
+        public int $mode = self::MODE_FIND_CLASSES,
+        iterable $filters = []
+    ) {
+        foreach ($filters as $filter) $this->addFilter($filter);
+    }
+
+    public function withFilter(FilterInterface $filter): self
+    {
+        $copy = new self($this->mode);
+        $copy->addFilter($filter);
+
+        return $copy;
+    }
+
+    /**
+     * @param iterable<FilterInterface> $filters
+     * @return self
+     */
+    public function withFilters(iterable $filters): self
+    {
+        $copy = new self($this->mode);
+        foreach ($filters as $filter) $copy->addFilter($filter);
+
+        return $copy;
+    }
+
+    private function addFilter(FilterInterface $filter): void
+    {
+        $this->filters[] = $filter;
+    }
+
     /**
      * @return \Generator<\ReflectionClass>
      * @throws \ReflectionException
      */
     public function find(string $dir): \Generator
+    {
+        $filters = $this->filters;
+        $classes = $this->createGenerator($dir);
+
+        while (($filter = array_shift($filters)) !== null) {
+            $classes = $filter->setClasses($classes);
+        }
+
+        foreach ($classes as $class) yield $class;
+    }
+
+    private function createGenerator(string $dir): \Generator
     {
         $parser = (new ParserFactory())->createForNewestSupportedVersion();
         $finder = new NodeFinder;
@@ -20,10 +76,7 @@ final class ClassFinder implements ClassFinderInterface
         $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
         $files = new \RegexIterator($files, '/\.php$/');
 
-        $filter = static function(Node $node): bool {
-            return ($node instanceof Node\Stmt\Class_
-                || $node instanceof Node\Stmt\Namespace_);
-        };
+        $filter = $this->createAstFilter();
 
         foreach ($files as $file) {
             $code = file_get_contents($file->getPathName());
@@ -50,9 +103,38 @@ final class ClassFinder implements ClassFinderInterface
                         )
                             : $node->name->toString();
                     }
+
                     yield new \ReflectionClass($cls);
                 }
             }
         }
+    }
+
+    private function createAstFilter(): callable
+    {
+        $nodes = [
+            Node\Stmt\Namespace_::class,
+        ];
+
+        if ($this->mode&self::MODE_FIND_CLASSES) {
+            array_unshift($nodes, Node\Stmt\Class_::class);
+        }
+        
+        if ($this->mode&self::MODE_FIND_INTERFACES) {
+            $nodes[] = Node\Stmt\Interface_::class;
+        }
+
+        if ($this->mode&self::MODE_FIND_ENUMS) {
+            $nodes[] = Node\Stmt\Enum_::class;
+        }
+
+        if ($this->mode&self::MODE_FIND_TRAITS) {
+            $nodes[] = Node\Stmt\Trait_::class;
+        }
+
+        return static function(Node $node) use ($nodes): bool {
+            foreach ($nodes as $n) if ($node instanceof $n) return true;
+            return false;
+        };
     }
 }
