6a35959a1cf196351a5c7f64fa5468741259d5ac
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-31 00:11:03 +0200

    Update ScannerFactory.php

--- src/ScannerFactory.php
+++ src/ScannerFactory.php
@@ -7,18 +7,11 @@ use function Bermuda\Config\conf;
 
 final class ScannerFactory
 {
-    public const fromContainer = '\Bermuda\ClassScanner\ScannerFactory::fromContainer';
-    public static function fromContainer(ContainerInterface $container): Scanner
+    public static function createFromContainer(ContainerInterface $container): Scanner
     {
-        $config = conf($container);
-        if ($config->offsetGet('app')->offsetExists(ConfigProvider::listeneresConfigKey)) {
-            $config = $config->offsetGet('app');
-            return new Scanner(
-                $config->get(ConfigProvider::listeneresConfigKey),
-                $container->get(ClassFinderInterface::class)
-            );
-        }
-
-        return new Scanner(finder: $container->get(ClassFinderInterface::class));
+        return new Scanner(
+            $container->get('config')->get(ConfigProvider::CONFIG_KEY_LISTENER, []),
+            $container->get(ClassFinderInterface::class)
+        );
     }
 }
