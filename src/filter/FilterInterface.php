5bbbda37905954dfe8de8042f0cf2533b99f3204
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-31 00:53:27 +0200

    Update FilterInterface.php

--- src/filter/FilterInterface.php
+++ src/filter/FilterInterface.php
@@ -2,10 +2,12 @@
 
 namespace Bermuda\ClassScanner\Filter;
 
+use Bermuda\Reflection\ReflectionClass;
+
 interface FilterInterface extends \IteratorAggregate
 {
     /**
-     * @param iterable<\ReflectionClass> $classes
+     * @param iterable<ReflectionClass> $classes
      */
     public function setClasses(iterable $classes): FilterInterface;
 }
