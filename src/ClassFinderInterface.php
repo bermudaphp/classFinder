995f31433686316c3de1e13d35b03acce27e4142
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-03 18:32:32 +0200

    Update ClassFinderInterface.php

--- src/ClassFinderInterface.php
+++ src/ClassFinderInterface.php
@@ -3,13 +3,14 @@
 namespace Bermuda\ClassScanner;
 
 use Bermuda\Reflection\ReflectionClass;
+use Bermuda\Reflection\ReflectionFunction;
 
 interface ClassFinderInterface
 {
     /**
      * @param string|string[] $dirs
      * @param string|string[] $exclude
-     * @return \Generator<ReflectionClass>
+     * @return \Generator<ReflectionClass|ReflectionFunction>
      * @throws \ReflectionException
      */
     public function find(string|array $dirs, string|array $exclude = []): iterable
